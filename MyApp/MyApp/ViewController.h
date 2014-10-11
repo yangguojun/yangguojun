6a63c4f9a913396ddd3838340a55480d557df752
yangguojun <yangguojunaccp@163.com>
2014-10-11 16:58:00 +0800

    message
    second commit

--- MyApp/MyApp/ViewController.h
+++ MyApp/MyApp/ViewController.h
@@ -9,5 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *lblSecond;
+@property (strong, nonatomic) IBOutlet UILabel *lblFirst;
 
 @end
